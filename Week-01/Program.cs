a735d98293a7d19f9763e87953c1c069682e6af9
Riaan Bhanushali <riaan.bhanushali24@sakec.ac.in>
2026-07-21 12:02:21 +0530

    Basic taking input and printing it

---
+++ Week-01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace First
+{
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine($"Welcome, {name}!");
+    }
+}
+}
