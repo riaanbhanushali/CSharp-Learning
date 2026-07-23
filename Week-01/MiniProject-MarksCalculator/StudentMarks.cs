f0f26fa8326368ea766f6ec2a077eae6b5fd97da
Riaan Bhanushali <riaan.bhanushali24@sakec.ac.in>
2026-07-23 13:48:34 +0530

    Mini Marks Calulator for students

---
+++ Week-01/MiniProject-MarksCalculator/StudentMarks.cs
@@ -0,0 +1,52 @@
+﻿using System ;
+
+namespace MarksCalculator
+{
+    class Calc
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter the number of Subjects");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+             int[] marks = new int[n];
+
+             Console.WriteLine("Enter the Marks");
+
+            InputMarks(marks);
+
+            int total = CalculateMarks(marks);
+
+            Console.WriteLine("Total : " + (CalculateMarks(marks)));
+
+            Console.WriteLine("Average : " + (CalculateAverage(total ,marks)) );
+
+
+
+        }
+
+        public static void InputMarks(int[] marks)
+        {
+            for(int i=0 ; i<marks.Length ; i++)
+            {
+                marks[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        public static int CalculateMarks(int[] marks)
+        {
+            int total = 0 ;
+            for(int i=0 ; i<marks.Length ; i++)
+            {
+                total += marks[i];
+            }
+            return total;
+        }
+
+        public static double CalculateAverage(int total , int[] marks)
+        {
+            double average = ((double)total/(marks.Length));
+            return average;
+        }
+    }
+}
