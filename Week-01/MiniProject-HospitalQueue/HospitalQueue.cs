0db39b24ac6deb6dd26d706ccb2a09c2c6a37ceb
Riaan Bhanushali <riaan.bhanushali24@sakec.ac.in>
2026-07-23 13:57:16 +0530

    Hospital Queue Mini Project

---
+++ Week-01/MiniProject-HospitalQueue/HospitalQueue.cs
@@ -0,0 +1,24 @@
+﻿using System ;
+using System.Collections.Generic;
+
+namespace HospitaQueueDemo
+{
+    class HospitalQueue
+    {
+        public static void Main()
+        {
+            Queue<string> Patients = new Queue<string>();
+            Patients.Enqueue("Riaan");
+            Patients.Enqueue("Dev");
+            Patients.Enqueue("Harsh");
+
+            Console.WriteLine("Now Serving : " + Patients.Dequeue());
+
+            foreach(string p in Patients)
+            {
+            Console.WriteLine(p);
+            }
+
+        }
+    }
+}
