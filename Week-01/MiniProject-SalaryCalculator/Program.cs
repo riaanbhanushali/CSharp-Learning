be78d04b9051af6dbd5ddc1e52e2bb08f75b435c
Riaan Bhanushali <riaan.bhanushali24@sakec.ac.in>
2026-07-23 13:44:37 +0530

    Add files via upload

---
+++ Week-01/MiniProject-SalaryCalculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EmployeeSalary
+{
+    class SalaryCalculator
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter your Salary:");
+            double salary = Convert.ToDouble(Console.ReadLine());
+
+            double tax = Taxes(salary);
+            Console.WriteLine("Tax deduction : " + tax );
+
+            double shares = Shares(salary);
+            Console.WriteLine("Shares Amount : " + shares);
+
+            double TotalInHand = InHand(salary, tax , shares);
+            Console.WriteLine("Total Money in Cash : " + (TotalInHand));
+        }
+        static double Taxes(double salary)
+        {
+            double tax = 0.05*(salary);
+            return tax;
+        }
+        static double Shares(double salary)
+        {
+            double shares = 0.4*(salary);
+            return shares;
+        }
+        static double InHand(double salary , double tax, double shares)
+        {
+            double finalAmount = salary-(tax + shares);
+            return finalAmount;
+        }
+    }
+}
