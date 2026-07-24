4a32c0fe852d0e1e8af79657e82f8d1fcbed74e3
Riaan Bhanushali <riaan.bhanushali24@sakec.ac.in>
2026-07-24 13:29:18 +0530

    Constructors Example Program

---
+++ Week-02/02-Constructors/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+namespace Constructors
+{
+    class Employee
+    {
+        public string Name = "";
+        public int Age;
+
+        public Employee()
+        {
+            Console.WriteLine("Constructor Executed");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Employee emp1 = new Employee();
+        }
+    }
+}
