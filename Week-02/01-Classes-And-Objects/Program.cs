d360eadd8c31edbf51f1fee0b5019d675bfd6406
Riaan Bhanushali <riaan.bhanushali24@sakec.ac.in>
2026-07-24 13:26:20 +0530

    Classes and Objects

---
+++ Week-02/01-Classes-And-Objects/Program.cs
@@ -0,0 +1,24 @@
+﻿using System ;
+
+namespace Job
+{
+    class Employee
+    {
+        public string Name="";
+        public int age ;
+    }
+
+     class Work()
+    {
+        public static void Main()
+        {
+        Employee emp1 = new Employee();
+
+        emp1.Name = "Riaan";
+        emp1.age = 21;
+
+        Console.WriteLine(emp1.Name);
+        Console.WriteLine(emp1.age);
+        }
+    }
+}
