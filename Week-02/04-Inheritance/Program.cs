0ad366ad173abe54e5497ba1fae14d8f275aa929
Riaan Bhanushali <riaan.bhanushali24@sakec.ac.in>
2026-07-27 12:35:59 +0530

    Inheritance Exmaple

---
+++ Week-02/04-Inheritance/Program.cs
@@ -0,0 +1,37 @@
+﻿using System ;
+
+namespace inheritance
+{
+    class Parent
+    {
+        public void Eyes()
+        {
+            Console.WriteLine("I have brown eyes");
+        }
+        public void Nose()
+        {
+            Console.WriteLine("I have a big nose ");
+        }
+    }
+
+    class Child : Parent
+    {
+        public void height()
+        {
+            Console.WriteLine("I am Tall");
+        }
+    }
+    
+    class Family
+    {
+        public static void Main()
+        {
+            Child c1 = new Child();
+            c1.Eyes();
+            c1.Nose();
+            c1.height();
+
+
+        }
+    }
+}
