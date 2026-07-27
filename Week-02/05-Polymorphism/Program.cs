19ba9ec2dff466bfb5a9341af6b0d97b00b90782
Riaan Bhanushali <riaan.bhanushali24@sakec.ac.in>
2026-07-27 12:38:16 +0530

    Polymorphism example

---
+++ Week-02/05-Polymorphism/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace polu
+{
+    class Animals
+    {
+        public virtual void sound()
+        {
+            Console.WriteLine("Animal makes a sound");
+        }
+    }
+    class Dog : Animals 
+    {
+        public override void sound()
+        {
+            Console.WriteLine("Dog Bark");
+        }
+    }
+    class Cat : Animals
+    {
+        public override void sound()
+        {
+            Console.WriteLine("Cat Meow");
+        }
+    }
+
+    class Program
+    {
+        public static void Main()
+        {
+        Dog d = new Dog();
+        Cat c = new Cat();
+
+        d.sound();
+        c.sound();
+        }
+    }
+}
