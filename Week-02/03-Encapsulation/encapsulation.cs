7a25811cb9c01c05e6cede7cdeb660314962e927
Riaan Bhanushali <riaan.bhanushali24@sakec.ac.in>
2026-07-27 12:29:16 +0530

    Encapsulation Example

---
+++ Week-02/03-Encapsulation/encapsulation.cs
@@ -0,0 +1,28 @@
+﻿using System ;
+using System.Collections.Generic ;
+
+namespace enscapsulation
+{
+    class Encap
+    {
+        private string name;
+        private int age ;
+
+        public Encap(string Name , int Age)
+        {
+            name = Name ;
+            age = Age;
+        }
+
+        public void display()
+        {
+            Console.WriteLine(name+":"+age);
+        }
+
+        static void Main()
+        {
+            Encap p1 = new Encap("Riaan", 19);
+            p1.display();
+        }
+    }
+}
