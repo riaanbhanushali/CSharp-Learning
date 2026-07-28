4e0d41c9eec6edc1f6b0b1e7665ed7f9f25b55b0
Riaan Bhanushali <riaan.bhanushali24@sakec.ac.in>
2026-07-28 11:07:07 +0530

    Collections Example

---
+++ Week-02/08-Collections/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace collectionDemo
+{
+    class StudentCollection
+    {
+        public static void Main()
+        {
+            List<string> Students = new List<string>();
+
+            Students.Add("Riaan");
+            Students.Add("Rahul");
+            Students.Add("Priya");
+
+            foreach(string s in Students)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine("\nRemoving Riaan ");
+
+            Students.Remove("Riaan");
+
+            foreach(string s in Students)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine("\nNumber of Students : " +(Students.Count));
+
+        }
+    }
+}
