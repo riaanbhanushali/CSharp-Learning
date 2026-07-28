c71554ee3f13902e1440daeb1a2a65eaf1449155
Riaan Bhanushali <riaan.bhanushali24@sakec.ac.in>
2026-07-28 11:09:13 +0530

    Generics Code

---
+++ Week-02/09-Generics/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenercisDemo
+{
+    class Box<T>
+    {
+        public T Item ;
+
+        public void display()
+        {
+            Console.WriteLine(Item);
+        }
+    }
+    class Program
+    {
+        public static void Main()
+        {
+            Box<string> books = new Box<string>();
+            books.Item="RB ka Book";
+            books.display();
+
+            Box<int> age = new Box<int>();
+            age.Item=19;
+            age.display();
+        }
+    }
+}
