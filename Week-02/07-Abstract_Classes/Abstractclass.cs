78577ff99195b22e3c5dd1b693934d21c4cc6416
Riaan Bhanushali <riaan.bhanushali24@sakec.ac.in>
2026-07-28 11:05:59 +0530

    Abstract Classes Example

---
+++ Week-02/07-Abstract_Classes/Abstractclass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace AbstractExample
+{
+    abstract class Employee
+    {
+        public void Login()
+        {
+            Console.WriteLine("Employee Logged In");
+        }
+
+        public abstract void Work();
+    }
+
+    class Developer : Employee
+    {
+        public override void Work()
+        {
+            Console.WriteLine("Writing Code");
+        }
+    }
+
+    class Tester : Employee
+    {
+        public override void Work()
+        {
+            Console.WriteLine("Testing Application");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Developer dev = new Developer();
+
+            dev.Login();
+            dev.Work();
+
+            Console.WriteLine();
+
+            Tester tester = new Tester();
+
+            tester.Login();
+            tester.Work();
+        }
+    }
+}
