08891f9583d3f7d4a4df5cddb9e55e04b1fcad95
Riaan Bhanushali <riaan.bhanushali24@sakec.ac.in>
2026-07-28 11:04:40 +0530

    Interfaces Example

---
+++ Week-02/06-Interfaces/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+
+namespace Interface
+{
+    interface IAnimal
+    {
+        void eat();
+        void sleep();
+        void run();
+    }
+
+    class Dog : IAnimal
+    {
+        public void eat()
+        {
+            Console.WriteLine("The dog is eating food");
+        }
+        public void sleep()
+        {
+            Console.WriteLine("The dog is sleeping");
+        }
+        public void run()
+        {
+            Console.WriteLine("The dog is running");
+        }
+    }
+
+    class Doggo
+    {
+        public static void Main()
+        {
+            Dog d = new Dog();
+
+            d.eat();
+            d.sleep();
+            d.run();
+        }
+    }
+}
