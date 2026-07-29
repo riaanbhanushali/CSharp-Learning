ed6d33dd3433a55aaa797ea2bec17c488c11ed5e
Riaan Bhanushali <riaan.bhanushali24@sakec.ac.in>
2026-07-29 16:50:06 +0530

    Vehicle Tracker System Code

---
+++ Week-02/MiniProject-Vehicle-Tracker/VehicleTracker.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Logistics
+{
+    class Vehicle
+    {
+        public virtual void Track()
+        {
+            Console.WriteLine("Tracking Vehicle");
+        }
+    }
+
+    class Truck : Vehicle
+    {
+        public override void Track()
+        {
+            Console.WriteLine("Tracking Truck");
+        }
+    }
+
+    class Bike : Vehicle
+    {
+        public override void Track()
+        {
+            Console.WriteLine("Tracking Bike");
+        }
+    }
+
+    class Van : Vehicle
+    {
+        public override void Track()
+        {
+            Console.WriteLine("Tracking Van");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Vehicle v1 = new Truck();
+            Vehicle v2 = new Bike();
+            Vehicle v3 = new Van();
+
+            v1.Track();
+            v2.Track();
+            v3.Track();
+        }
+    }
+}
