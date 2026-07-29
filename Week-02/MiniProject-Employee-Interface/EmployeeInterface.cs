767879d483ad6b66d1b3c22c07762e122ba32293
Riaan Bhanushali <riaan.bhanushali24@sakec.ac.in>
2026-07-29 15:13:10 +0530

    Employee Interface Code

---
+++ Week-02/MiniProject-Employee-Interface/EmployeeInterface.cs
@@ -0,0 +1,58 @@
+﻿using System ;
+
+namespace EmployeeManagement
+{
+    interface Iemployee
+    {
+        void Role();
+    }
+
+    class Jdeveloper : Iemployee
+    {
+        public void Role()
+        {
+            Console.WriteLine("THere are 25 Junior Developers Present");
+        }
+    }
+
+    class Sdeveloper : Iemployee
+    {
+        public void Role()
+        {
+            Console.WriteLine("There are 11 Senior Developers Present");
+        }
+    }
+
+    class HR : Iemployee
+    {
+        public void Role()
+        {
+            Console.WriteLine("There is 1 HR present in the company");
+        }
+    }
+
+    class RoleManager
+    {
+        private Iemployee employee;
+
+        public RoleManager(Iemployee employee)
+        {
+            this.employee = employee;
+        }
+
+        public void Manage()
+        {
+            employee.Role();
+        }
+    }
+
+    class MainP
+    {
+        public static void Main()
+        {
+        RoleManager role = new RoleManager(new Sdeveloper());
+
+        role.Manage();
+        }
+    }
+}
