950ae066287d13a9fdb419e7de8f834dcdb143a4
Riaan Bhanushali <riaan.bhanushali24@sakec.ac.in>
2026-07-29 16:48:31 +0530

    Notification Service Code

---
+++ Week-02/MiniProject-Notification-System/NotificationService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace NotificationSystem
+{
+    interface INotification
+    {
+        void Send(string message);
+    }
+
+    class EmailNotification : INotification
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine("Email Recieved : " + message);
+        }
+    }
+    class Whatsapp : INotification
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine("Whatsapp message Recieved : " + message);
+        }
+    }
+
+    class Teams : INotification
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine("Teams Message Recieved :" + message);
+        }
+    }
+
+    class NotificationService
+    {
+        private INotification notification;
+        public NotificationService(INotification notification)
+        {
+            this.notification = notification;
+        }
+
+        public void Notify(string message)
+        {
+            notification.Send(message);
+        }
+    }
+
+    class MainP
+    {
+        public static void Main()
+        {
+            NotificationService service = new NotificationService(new EmailNotification());
+
+            service.Notify("Salary Credited :) ");
+        }
+    }
+}
